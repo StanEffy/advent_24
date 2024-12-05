61d1db346d9547bc000c4b14f05778e9dc1ee591
StanEffy <holydonk@gmail.com>
2024-12-05 10:06:51 +0200

    day_3_1

--- day_3/solution_1.js
+++ day_3/solution_1.js
@@ -5,21 +5,25 @@ function parseTextFileToArray(filePath) {
     try {
         const fileContent = fs.readFileSync(filePath, 'utf8');
         
-        const regex = /mul\((-?\d+),(-?\d+)\)/g;
-  
+        let mulEnabled = true;
+  const results = [];
 
-        const matches = [];
-        
-        let match;
-        
-        while ((match = regex.exec(fileContent)) !== null) {
-            matches.push({
-            x: parseInt(match[1]),
-            y: parseInt(match[2]),
-            });
-        }
-        
-        return matches;
+  const regex = /mul\((-?\d+),(-?\d+)\)|do\(\)|don't\(\)/g;
+  
+  let match;
+  while ((match = regex.exec(fileContent)) !== null) {
+    if (match[0] === 'do()') {
+      mulEnabled = true;
+    } else if (match[0] === 'don\'t()') {
+      mulEnabled = false;
+    } else if (mulEnabled) {
+      const x = parseInt(match[1]);
+      const y = parseInt(match[2]);
+      results.push(x * y);
+    }
+  }
+
+  return results.reduce((sum, val) => sum + val, 0);
     } catch (error) {
         console.error('Error reading the file:', error.message);
         return null;
@@ -30,4 +34,4 @@ const inputFilePath = path.join(__dirname, 'input.txt');
 
 const result = parseTextFileToArray(inputFilePath);
 
-console.log(result.reduce((prev, cur) => prev += cur.x * cur.y, 0));
+console.log(result);
